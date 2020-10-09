14348bf8231de697e825228075fe0262d31a2242
Myst <1592048+Mystou@users.noreply.github.com>
2020-10-09 22:51:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
