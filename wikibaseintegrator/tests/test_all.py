4d36dbd69ea58dd52b42808a589d3b673a2cf3f3
Myst <1592048+Mystou@users.noreply.github.com>
2020-11-08 20:57:13 +0100

    Update test_all.py

--- wikibaseintegrator/tests/test_all.py
+++ wikibaseintegrator/tests/test_all.py
@@ -14,8 +14,7 @@ __license__ = 'AGPLv3'
 class TestMediawikiApiCall(unittest.TestCase):
     def test_all(self):
         with self.assertRaises(MWApiError):
-            wbi_core.ItemEngine.mediawiki_api_call("GET", "http://www.wikidataaaaaaaaa.org",
-                                                   max_retries=3, retry_after=1,
+            wbi_core.ItemEngine.mediawiki_api_call("GET", "http://www.wikidataaaaaaa.org", max_retries=3, retry_after=1,
                                                    params={'format': 'json', 'action': 'wbgetentities', 'ids': 'Q42'})
         with self.assertRaises(requests.HTTPError):
             wbi_core.ItemEngine.mediawiki_api_call("GET", "http://httpbin.org/status/400", max_retries=3, retry_after=1)
