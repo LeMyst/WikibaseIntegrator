eab4dd615b826c17d8f31bdcb7faf57c15c66fcb
Myst <1592048+Mystou@users.noreply.github.com>
2020-11-11 10:04:43 +0100

    Update wbi_core.py
    Add get_linked_by function
    
    Co-Authored-By: andrawaag <770306+andrawaag@users.noreply.github.com>

--- wikibaseintegrator/wbi_core.py
+++ wikibaseintegrator/wbi_core.py
@@ -1141,6 +1141,31 @@ class FunctionsEngine(object):
         return json_data
 
     @staticmethod
+    def get_linked_by(qid, mediawiki_api_url=None):
+        """
+            :param qid: Wikidata identifier to which other wikidata items link
+            :param mediawiki_api_url: default to wikidata's api, but can be changed to any wikibase
+            :return:
+        """
+
+        mediawiki_api_url = config['MEDIAWIKI_API_URL'] if mediawiki_api_url is None else mediawiki_api_url
+
+        linkedby = []
+        whatlinkshere = json.loads(requests.get(
+            mediawiki_api_url + "?action=query&list=backlinks&format=json&bllimit=500&bltitle=" + qid).text)
+        for link in whatlinkshere["query"]["backlinks"]:
+            if link["title"].startswith("Q"):
+                linkedby.append(link["title"])
+        while 'continue' in whatlinkshere.keys():
+            whatlinkshere = json.loads(requests.get(
+                mediawiki_api_url + "?action=query&list=backlinks&blcontinue=" +
+                whatlinkshere['continue']['blcontinue'] + "&format=json&bllimit=500&bltitle=" + qid).text)
+            for link in whatlinkshere["query"]["backlinks"]:
+                if link["title"].startswith("Q"):
+                    linkedby.append(link["title"])
+        return linkedby
+
+    @staticmethod
     @wbi_backoff()
     def execute_sparql_query(query, prefix=None, endpoint=None, user_agent=None, as_dataframe=False, max_retries=1000,
                              retry_after=60, debug=False):
