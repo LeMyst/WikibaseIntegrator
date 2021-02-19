e1896373a08105469f0cf31e32016856f45504cd
Myst <1592048+LeMyst@users.noreply.github.com>
2021-02-19 21:33:38 +0100

    Update test_wbi_core.py
    Re-add test

--- test/test_wbi_core.py
+++ test/test_wbi_core.py
@@ -141,7 +141,8 @@ class TestWbiCore(unittest.TestCase):
             wbi_core.Lexeme("123", prop_nr="P15"),
             wbi_core.Lexeme(123, prop_nr="P15"),
             wbi_core.Form("L123-F123", prop_nr="P16"),
-            wbi_core.Sense("L123-S123", prop_nr="P17")
+            wbi_core.Sense("L123-S123", prop_nr="P17"),
+            wbi_core.LocalMedia("DemoCat 2.png", prop_nr="P18")
         ]
         core_props = set(["P{}".format(x) for x in range(20)])
 
