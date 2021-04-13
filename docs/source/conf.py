11c1936e9a1e8abd309b993bfed22aafd19f200a
Myst <1592048+LeMyst@users.noreply.github.com>
2021-04-13 11:37:48 +0200

    Change theme to sphinx_rtd_theme
    Add Github links support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
+              'sphinx_rtd_theme',
               'sphinx_github_changelog']
 
 # Provide a GitHub API token:
@@ -80,90 +81,18 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
-    'description': "Easy to access and parse data from Wikipedia",
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': False,
+}
+
+html_context = {
+    'display_github': True,
     'github_user': 'LeMyst',
     'github_repo': 'WikibaseIntegrator',
-    'fixed_sidebar': True,
-    'github_banner': True,
-    'sidebar_width': '250px',
-    # 'page_width': '1200px',
-    'font_size': '17px',  # default value
-    'extra_nav_links': {},
+    'github_version': 'master',
+    "conf_py_path": "/docs/"
 }
-
-# -- Options for HTMLHelp output ------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'WikibaseIntegratordoc'
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'WikibaseIntegrator.tex', u'WikibaseIntegrator Documentation', u'Taras Lehinevych', 'manual'),
-]
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'wikibaseintegrator', u'WikibaseIntegrator Documentation', [author], 1)
-]
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'WikibaseIntegrator', u'WikibaseIntegrator Documentation', author, 'WikibaseIntegrator', 'One line description of project.', 'Miscellaneous'),
-]
