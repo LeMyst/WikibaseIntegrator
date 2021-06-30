225aa2a353ef96515b72dc8fc0ebfc33378b0924
Myst <1592048+LeMyst@users.noreply.github.com>
2021-06-30 20:06:19 +0200

    Use String Class for some datatypes

--- wikibaseintegrator/datatypes/commonsmedia.py
+++ wikibaseintegrator/datatypes/commonsmedia.py
@@ -1,40 +1,8 @@
-from wikibaseintegrator.datatypes.basedatatype import BaseDataType
+from wikibaseintegrator.datatypes.string import String
 
 
-class CommonsMedia(BaseDataType):
+class CommonsMedia(String):
     """
     Implements the Wikibase data type for Wikimedia commons media files
     """
     DTYPE = 'commonsMedia'
-
-    def __init__(self, value, **kwargs):
-        """
-        Constructor, calls the superclass BaseDataType
-        :param value: The media file name from Wikimedia commons to be used as the value
-        :type value: str or None
-        :param prop_nr: The item ID for this claim
-        :type prop_nr: str with a 'P' prefix followed by digits
-        :param snaktype: The snak type, either 'value', 'somevalue' or 'novalue'
-        :type snaktype: str
-        :param references: List with reference objects
-        :type references: A data type with subclass of BaseDataType
-        :param qualifiers: List with qualifier objects
-        :type qualifiers: A data type with subclass of BaseDataType
-        :param rank: rank of a snak with value 'preferred', 'normal' or 'deprecated'
-        :type rank: str
-        """
-
-        self.value = None
-
-        super(CommonsMedia, self).__init__(value=value, **kwargs)
-
-        self.set_value(value)
-
-    def set_value(self, value):
-        assert isinstance(value, str) or value is None, "Expected str, found {} ({})".format(type(value), value)
-        self.value = value
-
-        self.mainsnak.datavalue = {
-            'value': self.value,
-            'type': 'string'
-        }
--- wikibaseintegrator/datatypes/musicalnotation.py
+++ wikibaseintegrator/datatypes/musicalnotation.py
@@ -1,38 +1,8 @@
-from wikibaseintegrator.datatypes.basedatatype import BaseDataType
+from wikibaseintegrator.datatypes.string import String
 
 
-class MusicalNotation(BaseDataType):
+class MusicalNotation(String):
     """
     Implements the Wikibase data type 'musical-notation'
     """
     DTYPE = 'musical-notation'
-
-    def __init__(self, value, **kwargs):
-        """
-        Constructor, calls the superclass BaseDataType
-        :param value: Values for that data type are strings describing music following LilyPond syntax.
-        :type value: str or None
-        :param prop_nr: The item ID for this claim
-        :type prop_nr: str with a 'P' prefix followed by digits
-        :param snaktype: The snak type, either 'value', 'somevalue' or 'novalue'
-        :type snaktype: str
-        :param references: List with reference objects
-        :type references: A data type with subclass of BaseDataType
-        :param qualifiers: List with qualifier objects
-        :type qualifiers: A data type with subclass of BaseDataType
-        :param rank: rank of a snak with value 'preferred', 'normal' or 'deprecated'
-        :type rank: str
-        """
-
-        super(MusicalNotation, self).__init__(value=value, **kwargs)
-
-        self.set_value(value)
-
-    def set_value(self, value):
-        assert isinstance(value, str) or value is None, "Expected str, found {} ({})".format(type(value), value)
-        self.value = value
-
-        self.mainsnak.datavalue = {
-            'value': self.value,
-            'type': 'string'
-        }
