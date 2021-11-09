e36afb84f34d9b277d2b7331b21d6b8f5064c49b
Myst <1592048+LeMyst@users.noreply.github.com>
2021-11-09 23:30:31 +0100

    Duplicate metadata for Github weird dependency graph

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+# https://github.com/pallets/flask/blob/main/setup.py
+setup(
+    name="wikibaseintegrator",
+    install_requires=[
+        "backoff ~= 1.11.1",
+        "mwoauth ~= 0.3.7",
+        "oauthlib ~= 3.1.1",
+        "requests ~= 2.26.0",
+        "simplejson ~= 3.17.5"
+    ],
+    extras_require={
+        "dev": ["pytest"],
+        "coverage": ["pytest-cov"],
+    },
+)
