e0c43ffa075e112772d5dc5df433cb62192606ec
Myst <1592048+LeMyst@users.noreply.github.com>
2022-01-23 15:16:33 +0100

    Add value in ValueError message (#276)

--- wikibaseintegrator/datatypes/time.py
+++ wikibaseintegrator/datatypes/time.py
@@ -58,7 +58,7 @@ class Time(BaseDataType):
             pattern = re.compile(r'^[+-][0-9]*-(?:1[0-2]|0[0-9])-(?:3[01]|0[0-9]|[12][0-9])T(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]Z$')
             matches = pattern.match(time)
             if not matches:
-                raise ValueError("Time time must be a string in the following format: '+%Y-%m-%dT%H:%M:%SZ'")
+                raise ValueError(f"Time value ({time}) must be a string in the following format: '+%Y-%m-%dT%H:%M:%SZ'")
 
             if precision not in WikibaseDatePrecision:
                 raise ValueError("Invalid value for time precision, see https://www.mediawiki.org/wiki/Wikibase/DataModel/JSON#time")
