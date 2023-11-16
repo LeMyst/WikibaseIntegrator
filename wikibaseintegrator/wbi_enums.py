3d59026c7267031e390d1d88472622ac54a1f32b
Lukas Günther <hallo@lguenther.com>
2023-11-16 20:49:37 +0100

    Add Missing EDTF and LocalMedia Enums (#633)
    * add edtf
    
    * also added localmedia

--- wikibaseintegrator/wbi_enums.py
+++ wikibaseintegrator/wbi_enums.py
@@ -18,12 +18,14 @@ class ActionIfExists(Enum):
 
 class WikibaseDatatype(Enum):
     COMMONSMEDIA = 'commonsMedia'
+    EDTF = 'edtf'
     EXTERNALID = 'external-id'
     FORM = 'wikibase-form'
     GEOSHAPE = 'geo-shape'
     GLOBECOORDINATE = 'globe-coordinate'
     ITEM = 'wikibase-item'
     LEXEME = 'wikibase-lexeme'
+    LOCALMEDIA = 'localMedia'
     MATH = 'math'
     MONOLINGUALTEXT = 'monolingualtext'
     MUSICALNOTATION = 'musical-notation'
